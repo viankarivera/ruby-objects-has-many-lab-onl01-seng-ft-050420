5e5906016a91cb306effe5095271c637b931f950
Vianka Rivera <viankalrivera@gmail.com>
2020-05-20 16:50:18 +0000

    Automatically backed up by Learn

--- lib/author.rb
+++ lib/author.rb
@@ -15,16 +15,12 @@ class Author
   end 
 
   def add_post(post)
-    @posts << post
     post.author = self
-    @@post_count += 1
   end
 
   def add_post_by_title(title)
     post = Post.new(title)
-    @posts << post
     post.author = self
-    @@post_count += 1
   end
 
   def self.post_count
