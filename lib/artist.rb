6c0d3f0d736734ddd5cac4c8a7b4218919086d67
Vianka Rivera <viankalrivera@gmail.com>
2020-05-20 16:52:59 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -23,8 +23,8 @@ class Artist
     song.artist = self
   end
 
-  def add_song_by_name(song)
-    song = Song.new(song)
+  def add_song_by_name(name)
+    song = Song.new(name)
     song.artist = self
   end
 
