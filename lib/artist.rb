549131a2a523bb8e0e460629173cbae23c51292d
Vianka Rivera <viankalrivera@gmail.com>
2020-05-20 15:15:41 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,24 +1,29 @@
 class Artist
-  attr_accessor :name
-  @@all = []
+
+  attr_accessor :name, :songs
+
+  @@song_count = 0
+
   def initialize(name)
     @name = name
-    @@all << self
+    @songs = []
   end
-  def self.all
-    @@all
-  end
+
   def add_song(song)
+    @songs << song
     song.artist = self
+    @@song_count += 1
   end
+
   def add_song_by_name(name)
     song = Song.new(name)
+    @songs << song
     song.artist = self
+    @@song_count += 1
   end
-  def songs
-    Song.all.select {|song| song.artist == self}
-  end
+
   def self.song_count
-    Song.all.count
+    @@song_count
   end
+
 end
