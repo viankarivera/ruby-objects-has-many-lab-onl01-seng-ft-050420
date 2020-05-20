8fea3826a86c9ab3f8599e4f04943e0cd455cbae
Vianka Rivera <viankalrivera@gmail.com>
2020-05-20 15:14:49 +0000

    Automatically backed up by Learn

--- lib/post.rb
+++ lib/post.rb
@@ -1,19 +1,8 @@
 class Post
 
   attr_accessor :title, :author
-
-  @@all = []
-
-  def self.all
-    @@all
-  end 
-
   def initialize(title)
     @title = title
-  end
-
-  def posts
-    Song.all.select {|song| song.artist == self}
   end
 
   def author_name
