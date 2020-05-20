f0c6f1b570ab10f1bd12aeea3e8ee367da1fc9d6
Vianka Rivera <viankalrivera@gmail.com>
2020-05-20 15:19:29 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,9 +1,16 @@
 class Song
   attr_accessor :name, :artist
 
+  @@all = []
+
   def initialize(name)
     @name = name
+    @@all << self 
   end
+
+  def self.all 
+    @@all 
+  end 
 
   def artist_name
     if @artist == nil
